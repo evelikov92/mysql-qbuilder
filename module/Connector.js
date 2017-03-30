3678cb67ae0f7c531d152b0c41b26ad97e75e99f
evelikov92 <evelikov92@gmail.com>
2017-03-30 13:32:25 -0700

    Add Connector to repository

---
+++ module/Connector.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql')
+
+module.exports = (dbSetting) => {
+  let connection = mysql.createConnection({
+    host: dbSetting.hostname,
+    user: dbSetting.username,
+    password: dbSetting.password,
+    database: dbSetting.database
+  })
+
+  connection.connect()
+}
