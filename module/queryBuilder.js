269d01d37f7dbc0ea1a35be468c28ca3316108de
evelikov92 <evelikov92@gmail.com>
2017-03-30 11:46:45 -0700

    1) Make the delete function for query builder 2) Make the update function for query builder

--- module/queryBuilder.js
+++ module/queryBuilder.js
@@ -1,27 +1,27 @@
 let _where,
-_select,
-_from,
-_orderBy,
-_groupBy,
-_limit,
-_skip,
-_join,
-_insert,
-_delete,
-_update,
-_command,
-_table,
-_option,
-_params
+  _select,
+  _from,
+  _orderBy,
+  _groupBy,
+  _limit,
+  _skip,
+  _join,
+  _insert,
+  _delete,
+  _update,
+  _command,
+  _table,
+  _option,
+  _params
 
 function returnSymbolParamsCount (columns) {
   let length = columns.length
   let symbols = Array(length).join('?, ')
-  let v = symbols.slice(0, -2)
-  return v
+  return symbols.slice(0, -2)
 }
 
 module.exports = {
+  
   select: function (columns) {
     if (Array.isArray(columns)) {
       _select = `SELECT ${columns.join(', ')}`
@@ -34,20 +34,21 @@ module.exports = {
     return this
   },
   from: function (table) {
-    if (table.length > 0) {
+    if (table.length > 0 && !Array.isArray(table)) {
       _from = ` FROM ${table}`
+      _table = table
+      return this
     } else {
       throw new Error('The table variable is required to be string!')
     }
-    return this
   },
   table: function (table) {
-    if (table.length > 0) {
+    if (table.length > 0 && !Array.isArray(table)) {
       _table = table
+      return this
     } else {
       throw new Error('The table variable is required to be string!')
     }
-    return this
   },
   addSelect: function (columns) {
     if (!_select) {
@@ -67,13 +68,33 @@ module.exports = {
       if (symbols.length > 0) {
         _insert = 'INSERT INTO `' + _table + '` ( ' + symbols + ' ) VALUES ( ' + symbols + ' )'
         _option = 'insert'
+        return this
       } else {
         throw new Error('The columns is empty!')
       }
     } else {
       throw new Error('The columns is required to be array!')
     }
-
+  },
+  delete: function () {
+    if (_table) {
+      _delete = `DELETE FROM ${_table}`
+      _option = 'delete'
+      return this
+    } else {
+      throw new Error('Is need to enter in which table is need to delete record!')
+    }
+  },
+  update: function (columns) {
+    if (!Array.isArray(columns)) {
+      throw new Error('The columns variable is required to be array!')
+    }
+    if (!_table) {
+      throw new Error('Is need to enter in which table is need to delete record!')
+    }
+    let cl = columns.map((cl) => { return cl + ' = ?, ' })
+    _update = `UPDATE ${_table} SET ${cl.join('').slice(0, -2)}`
+    _option = 'update'
     return this
   }
 }
