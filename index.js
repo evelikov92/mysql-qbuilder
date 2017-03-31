b5a77f6ae5f75af891b4089c153f3c4648fcc947
evelikov92 <evelikov92@gmail.com>
2017-03-31 02:15:51 -0700

    Change the index file to work like another modules

--- index.js
+++ index.js
@@ -1,50 +1,104 @@
-// const builder = require('mysql-qbuilder')
-// let builder = builder({
-//   hostname: '',
-//   username: '',
-//   password: '',
-//   database: ''
-// })
-
 const mysql = require('mysql')
 const qBuilder = require('./module/QueryBuilder')
 
+let connection = null
 let _query = ''
 let _params = []
 
-module.exports = (dbSetting) => {
-  let connection = mysql.createConnection({
+/**
+ * [setOptions description]
+ * @param {[type]} dbSetting [description]
+ */
+exports.setOptions = (dbSetting) => {
+  connection = mysql.createConnection({
     host: dbSetting.hostname,
     user: dbSetting.username,
     password: dbSetting.password,
     database: dbSetting.database
   })
+}
 
-  return {
-    makeQuery: () => {
-      return qBuilder
-    },
-    prepare: () => {
-      _query = qBuilder.prepare()
-    },
-    setParameters: (params) => {
-      _params = params
-      qBuilder.setParameters(params)
-    },
-    getParameters: () => {
-      _params = qBuilder.getParameters()
-    },
-    execute: () => {
-      // connection.query(_query)
-    },
-    get: () => {
+/**
+ * [Start to create SQL Query]
+ * @return {QueryBuilder} [Query Builder Creator of sql queries and connect to database]
+ */
+exports.makeQuery = () => {
+  return qBuilder
+}
 
-    },
-    connectToDatabase: () => {
-      connection.connect()
-    },
-    endConnection: () => {
-      connection.end()
-    }
-  }
+/**
+ * [Prepare the sql query and set what kind of query is will used]
+ * @return {QueryBuilder} [Query Builder Creator of sql queries and connect to database]
+ */
+exports.prepare = () => {
+  _query = qBuilder.prepare()
+  return this
+}
+
+/**
+ * [Set parameters for created sql query]
+ * @param {Array} params  [description]
+ * @return {QueryBuilder} [Query Builder Creator of sql queries and connect to database]
+ */
+exports.setParameters = (params) => {
+  qBuilder.setParameters(params)
+  _params = params
+  return this
+}
+
+/**
+ * [Get all sql query parameters]
+ * @return {Array} [SQL Query parameters]
+ */
+exports.getParameters = () => {
+  _params = qBuilder.getParameters()
+  return _params
+}
+
+/**
+ * [Get the created sql query]
+ * @return {String} [SQL Query]
+ */
+exports.getCommand = () => {
+  return _query
+}
+
+/**
+ * [Set Your own sql query]
+ * @param  {String} command [SQL Query]
+ * @return {QueryBuilder}   [Query Builder Creator of sql queries and connect to database]
+ */
+exports.setCommand = function (command) {
+  _query = command
+  return this
+}
+
+/**
+ * [Execute the created sql query]
+ * @return {QueryBuilder}   [Query Builder Creator of sql queries and connect to database]
+ */
+exports.execute = () => {
+  return this
+}
+
+/**
+ * [Get the result from created sql query]
+ * @return {[type]} [description]
+ */
+exports.getResult = () => {
+
+}
+
+/**
+ * [Connect to MySQL server]
+ */
+exports.connectToDatabase = () => {
+  connection.connect()
+}
+
+/**
+ * [Close the MySQL connection]
+ */
+exports.closeTheConnection = () => {
+  connection.end()
 }
