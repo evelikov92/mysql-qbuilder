74a3b28ce50599d7dc56ac71a375edb0d5ca706e
evelikov92 <evelikov92@gmail.com>
2017-04-28 03:24:38 -0700

    Make the more unit tests and change the function for can work and with strings

--- test/common-tests.js
+++ test/common-tests.js
@@ -1,23 +1,52 @@
-let returnSymbolParamsCount = require('../lib/utilities/Common').returnSymbolParamsCount
+let common = require('../lib/utilities/Common')
 let expect = require('chai').expect
 
 describe('Test Return ? For Params', function () {
   it('should return ?, ? for "title, name"', function () {
-    expect(returnSymbolParamsCount('title, name')).equal('?, ?')
+    expect(common.returnSymbolParamsCount('title, name')).equal('?, ?')
   })
 })
 describe('Test Return ? For Params', function () {
   it('should return ? for "title, ', function () {
-    expect(returnSymbolParamsCount('title, ')).equal('?')
+    expect(common.returnSymbolParamsCount('title, ')).equal('?')
+  })
+})
+describe('Test Return ? For Params', function () {
+  it('should return "" for ", ', function () {
+    expect(common.returnSymbolParamsCount(', ')).to.throw(new Error('The columns is not recognized!'))
   })
 })
 describe('Test Return ? For Params', function () {
   it('should return ? for ["title", ""]', function () {
-    expect(returnSymbolParamsCount(['title', ''])).equal('?')
+    expect(common.returnSymbolParamsCount(['title', ''])).equal('?')
   })
 })
 describe('Test Return ? For Params', function () {
   it('should return ?, ?, ? for ["title", "test", "test-2"]', function () {
-    expect(returnSymbolParamsCount(['title', 'test', 'test-2'])).equal('?, ?, ?')
+    expect(common.returnSymbolParamsCount(['title', 'test', 'test-2'])).equal('?, ?, ?')
+  })
+})
+
+describe('Test Return ? For Params', function () {
+  it('should return "" for ["", "", ""]', function () {
+    expect(common.returnSymbolParamsCount(['', '', ''])).to.throw(new Error('The columns is not recognized!'))
+  })
+})
+
+describe('Test Return ? For Params', function () {
+  it('should return "" for []', function () {
+    expect(common.returnSymbolParamsCount([])).to.throw(new Error('The columns is not recognized!'))
+  })
+})
+
+describe('Test Return ? For Params', function () {
+  it('should return "" for ["", null, undefined]', function () {
+    expect(common.returnSymbolParamsCount(['', null, undefined])).to.throw(new Error('The columns is not recognized!'))
+  })
+})
+
+describe('Test Return ? For Params', function () {
+  it('should return "" for ["", " ", ""]', function () {
+    expect(common.returnSymbolParamsCount(['', ' ', ''])).to.throw(new Error('The columns is not recognized!'))
   })
 })
